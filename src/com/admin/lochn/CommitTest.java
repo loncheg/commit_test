567551a59d6f2763e8637f0fb8e80931b1a40f9b
lochn <1508282753@qq.com>
2021-05-08 21:02:23 +0800

    迭代一

---
+++ src/com/admin/lochn/CommitTest.java
@@ -0,0 +1,11 @@
+package com.admin.lochn;
+
+/**
+ * @author lochn
+ * @date 2021/5/8 - 21:01
+ */
+public class CommitTest {
+    public static void main(String[] args) {
+        System.out.println("给我提交成功啊喂！");
+    }
+}
